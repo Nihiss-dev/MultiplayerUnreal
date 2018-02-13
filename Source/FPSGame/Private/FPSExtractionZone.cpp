5e13afc109c4c325feb7ea8324d22d9e3c3f5af9
Nounours <alexis.feldmar@epitech.eu>
2018-02-13 18:38:33 +0100

    Adding mission end

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -3,6 +3,8 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -43,4 +45,14 @@ void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent *OverlappedComponent,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
 {
 	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone"));
+
+	AFPSCharacter *MyPawn = Cast<AFPSCharacter>(OtherActor);
+	if (MyPawn && MyPawn->bIsCarryingObjective)
+	{
+		AFPSGameMode *GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM)
+		{
+			GM->CompleteMission(MyPawn);
+		}
+	}
 }
